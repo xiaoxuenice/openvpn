74e97dc974e395a094b5a93c13c77ed8cab1f59f
xuewencang <xiaoxuenice@qq.com>
2020-04-29 14:08:21 +0800

    latest

---
+++ create-openvpn-user.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+if [  $# -eq 1 ];then
+echo -e "\033[47;31m Create user key Start..........  \033[0m"
+else
+echo -e "\033[47;31m Please add 'username' ......... \033[0m"
+exit
+fi
+cd /etc/openvpn/easy-rsa/3
+./easyrsa build-client-full $1 nopass
+mkdir -p /vpn-client/${1}
+cp pki/ca.crt /vpn-client/$1/
+cp pki/issued/${1}.crt /vpn-client/$1/
+cp pki/private/${1}.key /vpn-client/$1/
+cd /vpn-client/${1}
+cat >> ${1}.ovpn <<EOF
+client
+dev tun
+proto tcp
+proto udp
+remote 192.168.116.220  369
+resolv-retry infinite
+redirect-gateway
+nobind
+persist-key
+persist-tun
+user nobody
+group nobody
+ca ca.crt
+cert ${1}.crt
+key ${1}.key
+comp-lzo
+verb 3
+sndbuf 393216 
+rcvbuf 393216
+EOF
+#systemctl restart openvpn@server.service
+echo ====================CREATE DONE=====================
